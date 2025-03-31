5ab535462cdd03f869117458509ea6d1f7728b93
kailua <kailua@project2by2.jp>
2025-03-31 16:54:32 +0900

    themes: overlay: Introduce CustomIcons

--- common.mk
+++ common.mk
@@ -3,3 +3,6 @@ include vendor/themes/fonts.mk
 
 # Icon Shapes
 include vendor/themes/iconshapes.mk
+
+# Custom Icons
+include vendor/themes/custom_icons.mk
---
+++ custom_icons.mk
@@ -0,0 +1,2 @@
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/themes/overlay/CustomIcons
