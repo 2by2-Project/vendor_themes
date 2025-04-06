ae02346479b83724a17bc548f4672a2a58799459
kailua <kailua@project2by2.jp>
2025-04-07 00:40:13 +0900

    fonts: Massive remove useless fonts

--- fonts.mk
+++ fonts.mk
@@ -3,16 +3,5 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/themes/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
     vendor/themes/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
-PRODUCT_PACKAGES += \
-    FontGoogleSansOverlay \
-    FontHarmonySansOverlay \
-    FontInterOverlay \
-    FontLinotteSourceOverlay \
-    FontManropeOverlay \
-    FontOnePlusSansOverlay \
-    FontOneplusSlateSourceOverlay \
-    FontRobotoOverlay \
-    FontUbuntuOverlay
-
 # Mejiro fonts
 $(call inherit-product, external/mejiro/fonts.mk)
